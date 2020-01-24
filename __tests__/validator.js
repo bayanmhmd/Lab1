9be8d5bdeaaa9fb053a5dc9718fd10802e82e07a
bayan <bayana.mhmd99@gmail.com>
2020-01-24 17:09:01 +0200

    still need readme

--- __tests__/validator.js
+++ __tests__/validator.js
@@ -65,7 +65,8 @@ describe('validator module performs basic validation of', () => {
     
 
   it('booleans', () => {
-    expect(true).toBeFalsy();
+    let bool = false;
+    expect(validator.isBoolean(bool)).toBeTruthy();
   });
 
   it('functions', () => {
@@ -77,26 +78,42 @@ describe('validator module performs basic validation of', () => {
 
 describe('validator module performs complex validations', () => {
 
-  it('validates the presence of required object properties at any level', () => {
-    // i.e. does person.hair.color exist and have a good value, not just person.hair
-    expect(true).toBeFalsy();
-  });
-
-  it('validates the proper types of object properties', () => {
-    // i.e. person.name must be a string, etc.
-    expect(true).toBeFalsy();
-  });
-
-  it('validates the types of values contained in an array', () => {
-    // i.e. an array of all strings or numbers
-    expect(true).toBeFalsy();
-  });
-
-  it('validates a value array against an approved list', () => {
-    // i.e. a string might only be allowed to be "yes" or "no"
-    expect(true).toBeFalsy();
-  });
-
-  // TODO: Cover so, so many more cases
-
+it('If the object is an empty or not', () => {
+  const susan = {
+    id:'123-45-6789',
+    name:'Susan McDeveloperson',
+    age: 37,
+    children:[],
+  }; 
+  expect(validator.objectKeys(susan)).toBeTruthy();
 });
+});
+describe('validator module performs complex validations', () => {
+
+const susan = {
+    id:'123-45-6789',
+    name:'Susan McDeveloperson',
+    age: 37,
+    children:[],
+  }; 
+
+    it('validates the presence of required object properties at any level', () => {
+      // i.e. does person.hair.color exist and have a good value, not just person.hair
+      expect(validator.notEmptyValues(susan)).toBeFalsy();
+    });
+  
+    it('validates the proper types of object properties', () => {
+      // i.e. person.name must be a string, etc.
+      expect(validator.typeValues(susan)).toBeTruthy();
+    });
+  
+    it('validates the types of values contained in an array', () => {
+      // i.e. an array of all strings or numbers
+            expect(validator.valueInArray(susan)).toBeFalsy();
+    });
+  
+    it('validates a value array against an approved list', () => {
+      // i.e. a string might only be allowed to be "yes" or "no"
+      expect(validator.noValueInArray(susan)).toBeFalsy();
+    });
+});  
